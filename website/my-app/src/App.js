55ca37fadba84cf5db5470e1266a3e9397bf52d4
Adalsteinn Jonsson <adalsteini@gmail.com>
2021-05-21 11:21:31 +0200

    Created the graphs inside of react

---
+++ website/my-app/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+  Center,
+  Flex,
+  Heading,
+} from '@chakra-ui/react';
+import BarChart from "./BarChart";
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Flex direction="column">
+            <Center h="100vh" w="100vw" bg="gray.400">
+              <Heading>Global Overview </Heading>
+              <BarChart />
+            </Center>
+            <Center h="200vh" w="100vw" bg="orange.400">
+              <Heading>Energy </Heading>
+            </Center>
+            <Center h="100vh" w="100vw" bg="red.400">
+              <Heading>Industry</Heading>
+            </Center>
+            <Center h="100vh" w="100vw" bg="green.400">
+              <Heading>Agriculture</Heading>
+            </Center>
+            <Center h="100vh" w="100vw" bg="blue.300">
+              <Heading>Transport</Heading>
+            </Center>
+            <Center h="100vh" w="100vw" bg="purple.300">
+              <Heading>My personal Emissions</Heading>
+            </Center>
+          </Flex>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
